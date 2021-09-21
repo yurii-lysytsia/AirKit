a29da18b00686a3bf68fedd16d558b7c652a2934
Yuri <yuri17fox@gmail.com>
2021-09-21 12:34:23 +0300

    Added new optional operator

--- AirKit.playground/Contents.swift
+++ AirKit.playground/Contents.swift
@@ -1,22 +1,36 @@
 import AirKit
 
+// MARK: - Operators
+
+infix operator ??=: AssignmentPrecedence
+infix operator ?=: AssignmentPrecedence
+
 public extension Optional {
-    
-    /// Runs a block to wrapped value if not nil.
+    /// Assign an optional value to a variable only if the value is not nil.
     ///
     ///     var string: String? = nil
-    ///     string.let { print("String is \($0)") } // Block didn't call because `string` is `nil`
+    ///     string ??= "Hello world!" // New value have been set | string == String("Hello world!")
+    ///     string ??= nil // New value haven't been set because it's `nil` | string == String("Hello world!")
     ///
-    ///     string = "Some text"
-    ///     string.let { print("String is \($0)") } // Printed `"String is Some text"`
+    ///     var dict = [String : String]()
+    ///     dict["hello"] ??= "world" // New value have been set | string == String("world")
+    ///     dict["hello"] ??= nil // New value haven't been set because it's `nil` | dict["hello"] == Stirng("world")
     ///
-    /// - Parameter block: A block to run if self is not nil.
-    func `let`(block: (Wrapped) -> Void) { if let value = self { block(value) } }
-    
+    static func ??= (lhs: inout Optional, rhs: Optional) { lhs = rhs ?? lhs }
+
+    /// Assign an optional value to a variable only if the variable is nil.
+    ///
+    ///     var string: String? = nil
+    ///     string ?= "Hello" // New value have been set because it was nil before | string == String("Hello")
+    ///     string ?= "World" // New value haven't been set because it's not nil | string == String("Hello")
+    ///
+    static func ?= (lhs: inout Optional, rhs: @autoclosure () -> Optional) {
+        if lhs == nil {
+            lhs = rhs()
+        }
+    }
 }
-//Сделать if - как метод чтобы его использовать inline. Так же сделать iflet метод и т.д
 
 var string: String? = nil
-string.let { print("String is \($0)") } // Block didn't call because `string` is `nil`
-string = "Some text"
-string.let { print("String is \($0)") } // Printed `"String is Some text"`
+string ?= "Hello" // New value have been set because it was nil before | string == String("Hello")
+string ?= "World" // New value haven't been set because it's not nil | string == String("Hello")
--- AirKit/Source/Swift/Optional+AirKit.swift
+++ AirKit/Source/Swift/Optional+AirKit.swift
@@ -58,3 +58,34 @@ public extension Optional {
     /// - Parameter block: A block to run if self is not nil.
     func `let`(block: (Wrapped) -> Void) { if let value = self { block(value) } }
 }
+
+// MARK: - Operators
+
+infix operator ??=: AssignmentPrecedence
+infix operator ?=: AssignmentPrecedence
+
+public extension Optional {
+    /// Assign an optional value to a variable only if the value is not nil.
+    ///
+    ///     var string: String? = nil
+    ///     string ??= "Hello world!" // New value have been set | string == String("Hello world!")
+    ///     string ??= nil // New value haven't been set because it's `nil` | string == String("Hello world!")
+    ///
+    ///     var dict = [String : String]()
+    ///     dict["hello"] ??= "world" // New value have been set | string == String("world")
+    ///     dict["hello"] ??= nil // New value haven't been set because it's `nil` | dict["hello"] == Stirng("world")
+    ///
+    static func ??= (lhs: inout Optional, rhs: Optional) { lhs = rhs ?? lhs }
+
+    /// Assign an optional value to a variable only if the variable is nil.
+    ///
+    ///     var string: String? = nil
+    ///     string ?= "Hello" // New value have been set because it was nil before | string == String("Hello")
+    ///     string ?= "World" // New value haven't been set because it's not nil | string == String("Hello")
+    ///
+    static func ?= (lhs: inout Optional, rhs: @autoclosure () -> Optional) {
+        if lhs == nil {
+            lhs = rhs()
+        }
+    }
+}
