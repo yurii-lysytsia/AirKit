be97f9cdedb9a42f6fe8c268bc5869cad242718c
Yuri <yuri17fox@gmail.com>
2021-09-21 12:20:34 +0300

    Added optional

--- AirKit.playground/Contents.swift
+++ AirKit.playground/Contents.swift
@@ -1,2 +1,22 @@
 import AirKit
 
+public extension Optional {
+    
+    /// Runs a block to wrapped value if not nil.
+    ///
+    ///     var string: String? = nil
+    ///     string.let { print("String is \($0)") } // Block didn't call because `string` is `nil`
+    ///
+    ///     string = "Some text"
+    ///     string.let { print("String is \($0)") } // Printed `"String is Some text"`
+    ///
+    /// - Parameter block: A block to run if self is not nil.
+    func `let`(block: (Wrapped) -> Void) { if let value = self { block(value) } }
+    
+}
+//Сделать if - как метод чтобы его использовать inline. Так же сделать iflet метод и т.д
+
+var string: String? = nil
+string.let { print("String is \($0)") } // Block didn't call because `string` is `nil`
+string = "Some text"
+string.let { print("String is \($0)") } // Printed `"String is Some text"`
--- AirKit/Source/Swift/Optional+AirKit.swift
+++ AirKit/Source/Swift/Optional+AirKit.swift
@@ -43,3 +43,18 @@ extension Optional where Wrapped == String {
     ///
     var isBlankOrNil: Bool { self?.isBlank != false }
 }
+
+// MARK: - Extensions | Conditions
+
+public extension Optional {
+    /// Runs a block to wrapped value if not nil.
+    ///
+    ///     var string: String? = nil
+    ///     string.let { print("String is \($0)") } // Block didn't call because `string` is `nil`
+    ///
+    ///     string = "Some text"
+    ///     string.let { print("String is \($0)") } // Printed `"String is Some text"`
+    ///
+    /// - Parameter block: A block to run if self is not nil.
+    func `let`(block: (Wrapped) -> Void) { if let value = self { block(value) } }
+}
