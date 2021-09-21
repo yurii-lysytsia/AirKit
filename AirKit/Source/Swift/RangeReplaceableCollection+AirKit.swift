a928547774c18b3ca9ddbaafbb8252603d75a04c
Yuri <yuri17fox@gmail.com>
2021-09-21 17:54:03 +0300

    RangeReplaceableCollection+AirKit

--- AirKit/Source/Swift/RangeReplaceableCollection+AirKit.swift
+++ AirKit/Source/Swift/RangeReplaceableCollection+AirKit.swift
@@ -49,11 +49,47 @@ extension RangeReplaceableCollection where Element: Equatable {
     ///     array.removeAll([1, 2]) // [3]
     ///
     mutating func removeAll(_ elements: [Element]) { removeAll { elements.contains($0) } }
+
+    /// Returns an array with removed all duplicate elements from the collection.
+    ///
+    ///     struct SomeStruct: Equatable {
+    ///         let value: Int
+    ///     }
+    ///
+    ///     [SomeStruct(value: 1), SomeStruct(value: 2), SomeStruct(value: 1)].removingDuplicates() // [{value 1}, {value 2}]
+    ///
+    func removingDuplicates() -> Self {
+        reduce(into: Self()) {
+            if $0.contains($1) { return }
+            $0.append($1)
+        }
+    }
+    
+    /// Removes all duplicate elements from the collection.
+    ///
+    ///     struct SomeStruct: Equatable {
+    ///         let value: Int
+    ///     }
+    ///
+    ///     var array: [SomeStruct] = [.init(value: 1), .init(value: 2), .init(value: 1)]
+    ///     array.removeDuplicates() // [{value 1}, {value 2}]
+    ///
+    mutating func removeDuplicates() { self = removingDuplicates() }
 }
 
 // MARK: - Extensions | Hashable
 
 public extension RangeReplaceableCollection where Element: Hashable {
+    /// Removes all duplicate elements from the collection.
+    ///
+    ///     [1, 2, 3, 2, 1].removingDuplicates() // [1, 2, 3]
+    ///
+    func removingDuplicates() -> Self {
+        var collection = self
+        collection.removeDuplicates()
+        return collection
+    }
+    
     /// Removes all duplicate elements from the collection.
     ///
     ///     var array = [1, 2, 3, 2, 1]
@@ -78,5 +114,19 @@ public extension RangeReplaceableCollection where Index: AdditiveArithmetic {
     ///
     mutating func prepend(_ newElement: Element) {
         insert(newElement, at: .zero)
+    }
+}
+
+// MARK: - Extensions | Index == Int
+
+public extension RangeReplaceableCollection where Index == Int {
+    /// Returns grouped collection by size from a given collection.
+    ///
+    ///     [1,2,3,4,5,6].grouping(by: 2) // [[1, 2], [3, 4], [5, 6]]
+    ///
+    func grouping(by size: Int) -> [Self] {
+        stride(from: 0, to: count, by: size).map {
+            Self(self[$0 ..< Swift.min($0 + size, count)])
+        }
     }
 }
