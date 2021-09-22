f7d588a333b46f4adae5b5d7c5dd1654ab2bc31c
Yuri <yuri17fox@gmail.com>
2021-09-22 18:57:06 +0300

    Added BinaryIntegerTests

---
+++ AirKitTests/Source/Swift/BinaryIntegerTests.swift
@@ -0,0 +1,15 @@
+//  Copyright © 2021 Yurii Lysytsia. All rights reserved.
+
+import XCTest
+@testable import AirKit
+
+class BinaryIntegerTests: XCTestCase {
+
+    func testToString() {
+        let value = 123
+        XCTAssert(value.toString() == "123")
+        XCTAssert(value.toString(integerDigits: 2) == "123")
+        XCTAssert(value.toString(integerDigits: 5) == "00123")
+    }
+
+}
