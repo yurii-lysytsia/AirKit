fc906a1845c5ad96f4c4ee31714fc8501fd931a9
Yuri <yuri17fox@gmail.com>
2021-09-24 12:07:32 +0300

    DictionaryTests

--- AirKitTests/Source/Swift/DictionaryTests.swift
+++ AirKitTests/Source/Swift/DictionaryTests.swift
@@ -4,13 +4,35 @@ import XCTest
 @testable import AirKit
 
 class DictionaryTests: XCTestCase {
-
+    
     func testRemoveAll() {
         var dict = ["key1" : "value1", "key2" : "value2", "key3" : "value3"]
         dict.removeAll(keys: ["key1", "key2"])
         XCTAssert(dict == ["key3": "value3"])
     }
-
+    
+    func testRemoveValueForRandomKey() {
+        var dict = ["key1" : "value1", "key2" : "value2", "key3" : "value3"]
+        let removedDictValue = dict.removeValueForRandomKey()
+        XCTAssert(removedDictValue != nil && !dict.values.contains(removedDictValue!))
+        
+        var emptyDict = [String: String]()
+        let removedEmptyDictValue = emptyDict.removeValueForRandomKey()
+        XCTAssert(removedEmptyDictValue == nil && emptyDict.isEmpty)
+    }
+    
+    func testMapKeysAndValues() {
+        let dict = [1 : 1, 2 : 2, 3 : 3]
+        let mappedDict = dict.mapKeysAndValues { ("key\($0)", "value\($1)") }
+        XCTAssert(mappedDict == ["key1" : "value1", "key2" : "value2", "key3" : "value3"])
+        
+        let dictNil = [1 : 1, 2 : 2, 3 : nil]
+        let mappedDictNil = dictNil.compactMapKeysAndValues { key, value in
+            return value.map { ("key\(key)", "value\($0)") }
+        }
+        XCTAssert(mappedDictNil == ["key1" : "value1", "key2" : "value2"])
+    }
+    
     func testOperators() throws {
         let expectedResult = ["key1": "value1", "key2": "value2"]
         let dict_0 = ["key1": "value1"]
@@ -22,5 +44,5 @@ class DictionaryTests: XCTestCase {
         dict += dict_1
         XCTAssert(dict == expectedResult)
     }
-
+    
 }
