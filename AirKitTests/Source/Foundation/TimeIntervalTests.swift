22b9064bc8c7a7614a2f641e667ff083818f7564
Yuri <yuri17fox@gmail.com>
2021-09-24 12:11:18 +0300

    TimeIntervalTests

--- AirKitTests/Source/Foundation/TimeIntervalTests.swift
+++ AirKitTests/Source/Foundation/TimeIntervalTests.swift
@@ -4,5 +4,12 @@ import XCTest
 @testable import AirKit
 
 class TimeIntervalTests: XCTestCase {
-
+    
+    func testValues() {
+        XCTAssert(TimeInterval.minutes(10) == 600)
+        XCTAssert(TimeInterval.hours(5) == 18_000)
+        XCTAssert(TimeInterval.days(3) == 259_200)
+        XCTAssert(TimeInterval.weeks(2) == 1_209_600)
+    }
+    
 }
