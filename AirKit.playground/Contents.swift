bd80b88c3e555fe381c13582ad71c3b76a9499da
Yuri <yuri17fox@gmail.com>
2021-09-24 14:22:47 +0300

    script

--- AirKit.playground/Contents.swift
+++ AirKit.playground/Contents.swift
@@ -1,15 +1,1 @@
 import AirKit
-
-extension Sequence where Element: Sequence {
-    
-    func reduce() -> [Element.Element] {
-        reduce([]) { $0 + $1 }
-    }
-    
-}
-
-let array = [["A"], ["B"], ["C"], ["D"]]
-array.reduce() // ["A", "B", "C", "D"]
-
-//let newArray = array.reduce([]) { $0 + $1 }
-//newArray
