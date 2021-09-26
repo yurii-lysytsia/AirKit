7cdc59247af68a12424e6728bfd278a8a4c4ab11
Yuri <yuri17fox@gmail.com>
2021-09-26 19:09:03 +0300

    Date formatter

--- AirKit.playground/Contents.swift
+++ AirKit.playground/Contents.swift
@@ -1,1 +1,87 @@
-import AirKit
+//import AirKit
+
+import Foundation
+
+
+//extension DateFormatter {
+//    /// If `date` in yesterday will return "Вчора" else if `date` in today will return "Сьогодні" else if `date` in tomorrow will return "Завтра". Instead will return `string(from:)`
+//    func string(relativeFrom date: Date) -> String {
+//        let calendar = Calendar.current
+//        if calendar.isDateInYesterday(date) {
+//            return L10n.RelativeDates.yesterday
+//        } else if calendar.isDateInToday(date) {
+//            return L10n.RelativeDates.today
+//        } else if calendar.isDateInTomorrow(date) {
+//            return L10n.RelativeDates.tomorrow
+//        } else {
+//            return string(from: date)
+//        }
+//    }
+//
+//    enum DateState {
+//        case today
+//        case tomorrow
+//        case yesterday
+//        case future
+//    }
+//
+//    func getDateState(date: Date) -> DateState {
+//        let calendar = Calendar.current
+//        if calendar.isDateInYesterday(date) {
+//            return .yesterday
+//        } else if calendar.isDateInToday(date) {
+//            return .today
+//        } else if calendar.isDateInTomorrow(date) {
+//            return .tomorrow
+//        } else {
+//            return .future
+//        }
+//    }
+//}
+
+
+
+//extension Date {
+//
+//    static var current: Date {
+//        return Date()
+//    }
+//
+//}
+
+//extension Date {
+//
+//    var string: String {
+//        let formatter = DateFormatter(dateStyle: .short)
+//        return formatter.string(from: self)
+//    }
+//
+//    func string(format: String) -> String {
+//        return DateFormatter.init(dateFormat: format).string(from: self)
+//    }
+//
+//    var stringTimeAndDate: String {
+//        let formatterTime = DateFormatter(dateStyle: .short, timeStyle: .short)
+//        return formatterTime.string(from: self)
+//    }
+//
+//}
+
+//extension String {
+//
+//    var serverDate: Date? {
+//        return DateFormatter(dateFormat: "yyyy-MM-dd'T'HH:mm:ss").date(from: self)
+//    }
+//
+//    var date: Date? {
+//        let formatter = DateFormatter(dateStyle: .short)
+//        return formatter.date(from: self)
+//    }
+//
+//    var dateAndTime: Date? {
+//        let formatterTime = DateFormatter(dateStyle: .short, timeStyle: .short)
+//        return formatterTime.date(from: self)
+//    }
+//
+//}
+
---
+++ AirKit/Source/Foundation/DateFormatter+AirKit.swift
@@ -0,0 +1,29 @@
+//  Copyright © 2021 Yurii Lysytsia. All rights reserved.
+
+import Foundation
+
+// MARK: - Extensions | Init
+
+public extension DateFormatter {
+    /// Create a new instance of formatter with date format string.
+    /// - Parameters:
+    ///   - dateFormat: The date format string used by the receiver.
+    ///   - timeZone: The time zone for the receiver. By default `.current`.
+    convenience init(dateFormat: String, timeZone: TimeZone = .current) {
+        self.init()
+        self.dateFormat = dateFormat
+        self.timeZone = timeZone
+    }
+    
+    /// Create a new instance of formatter with date and time style string.
+    /// - Parameters:
+    ///   - dateStyle: The date style of the receiver. By default is `none`
+    ///   - timeStyle: The time style of the receiver. By default is `none`
+    ///   - timeZone: The time zone for the receiver. By default is `.current`.
+    convenience init(dateStyle: DateFormatter.Style = .none, timeStyle: DateFormatter.Style = .none, timeZone: TimeZone = .current) {
+        self.init()
+        self.dateStyle = dateStyle
+        self.timeStyle = timeStyle
+        self.timeZone = timeZone
+    }
+}
