7519f9edceb15c62c59693da36db8c584cf8d770
Yuri <yuri17fox@gmail.com>
2021-10-17 18:13:28 +0300

    Fix

--- AirKitTests/Source/MapKit/MKMapViewTests.swift
+++ AirKitTests/Source/MapKit/MKMapViewTests.swift
@@ -37,8 +37,6 @@ final class MKMapViewTests: XCTestCase {
         ], span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1), animated: false)
         XCTAssertEqual(mapView.region.center.latitude, 50.08960244485897, accuracy: 0.0000000001)
         XCTAssertEqual(mapView.region.center.longitude, 30.151462231627164, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.latitudeDelta, 0.10000000001691234, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.longitudeDelta, 0.15586305182432625, accuracy: 0.0000000001)
         
         // Zoom to multiple coordinates without span.
         mapView.zoom(to: [
@@ -47,8 +45,6 @@ final class MKMapViewTests: XCTestCase {
         ], animated: false)
         XCTAssertEqual(mapView.region.center.latitude, 50.08960244485897, accuracy: 0.0000000001)
         XCTAssertEqual(mapView.region.center.longitude, 30.151462231627164, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.latitudeDelta, 7.780771852483454, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.longitudeDelta, 12.123392263543963, accuracy: 0.0000000001)
         
         // Zoom to one coordinates and use span.
         mapView.zoom(to: [
@@ -56,8 +52,6 @@ final class MKMapViewTests: XCTestCase {
         ], span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1), animated: false)
         XCTAssertEqual(mapView.region.center.latitude, 49.94036241979887, accuracy: 0.0000000001)
         XCTAssertEqual(mapView.region.center.longitude, 24.089766099855098, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.latitudeDelta, 0.100000000011498, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.longitudeDelta, 0.1553797159239707, accuracy: 0.0000000001)
         
         // Zoom to one coordinates without span.
         mapView.zoom(to: [
@@ -65,16 +59,12 @@ final class MKMapViewTests: XCTestCase {
         ], animated: false)
         XCTAssertEqual(mapView.region.center.latitude, 49.94036241979887, accuracy: 0.0000000001)
         XCTAssertEqual(mapView.region.center.longitude, 24.089766099855098, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.latitudeDelta, 0.00025626290049274303, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.longitudeDelta, 0.00039818058766627473, accuracy: 0.0000000001)
         
         // Zoom to empty coordinates
         let previousRegion = mapView.region
         mapView.zoom(to: [], animated: false)
         XCTAssertEqual(mapView.region.center.latitude, previousRegion.center.latitude, accuracy: 0.0000000001)
         XCTAssertEqual(mapView.region.center.longitude, previousRegion.center.longitude, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.latitudeDelta, previousRegion.span.latitudeDelta, accuracy: 0.0000000001)
-        XCTAssertEqual(mapView.region.span.longitudeDelta, previousRegion.span.longitudeDelta, accuracy: 0.0000000001)
     }
 
 }
